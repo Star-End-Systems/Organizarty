147e429352efff9ee4ed5837f6c961932b075e2b
GustavoPisanello <99992149+GustavoPisanello@users.noreply.github.com>
2023-11-01 23:28:48 +0000

    feat: Changes

--- Organizarty.UI/Pages/Accounts/Login.cshtml.cs
+++ Organizarty.UI/Pages/Accounts/Login.cshtml.cs
@@ -2,8 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Organizarty.UI.Pages.Accounts;
 
-public class LoginModel : PageModel
-{
+public class LoginModel : PageModel {
 
     public void OnGet() { } }
 
