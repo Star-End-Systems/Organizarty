e0c44803961e446c273e58aefa8eb1b99e5c7879
Anghie <87211689+Anghievisck@users.noreply.github.com>
2023-11-12 04:31:09 +0000

    Feat: Ending the Client area

---
+++ Organizarty.UI/Pages/Clients/Services.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+namespace Organizarty.UI.Pages;
+
+public class ServicesModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public ServicesModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+    
+    public InputModel Input{get; set;} = default!;
+
+    public class InputModel{
+  
+        [Display(Name = "Search")]
+        public string Search {get; set;} = default!;
+    }
+    public void OnGet()
+    {
+
+    }
+        public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        return RedirectToPage("", new { search = Input.Search});
+    }        
+}
