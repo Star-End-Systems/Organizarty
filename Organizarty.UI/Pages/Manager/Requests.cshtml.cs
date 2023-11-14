667ceaf3ccafdd38006484c7de07013fa6136e4c
Anghie <87211689+Anghievisck@users.noreply.github.com>
2023-11-14 21:49:28 +0000

    feat: Requests done

---
+++ Organizarty.UI/Pages/Manager/Requests.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+namespace Organizarty.UI.Pages;
+
+public class RequestsManagerModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public RequestsManagerModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+    public InputModel Input{get; set;} = default!;
+    public class InputModel{
+        [Required]
+        [Display(Name = "Search")]
+        public string Search {get; set;} = default!;
+    }
+    public void OnGet()
+    {
+
+    }
+        public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        return RedirectToPage("", new { search = Input.Search});
+    }        
+}
