98139041c256957478a16194a6a1d84f1cf67f09
lu-css <dossantossilvalucas963@gmail.com>
2023-11-16 21:24:35 -0300

    add razor pages

--- Organizarty.UI/Program.cs
+++ Organizarty.UI/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
 // Add services to the container.
 builder.Services.AddDatabase(builder.Configuration);
@@ -43,6 +44,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.UseAuthorization();
 
