ba5a48537c3a63639edbb7a13587e5836290e993
lu-css <dossantossilvalucas963@gmail.com>
2023-12-02 00:36:04 -0300

    fix api auth

--- Organizarty.UI/Controllers/ScheduleController.cs
+++ Organizarty.UI/Controllers/ScheduleController.cs
@@ -41,7 +41,7 @@ public class ScheduleController : ControllerBase
     [HttpGet("Orders")]
     public async Task<ActionResult<ManyItems<OrderResponse>>> GetOrders()
     {
-        var user = await _authHelper.GetUserFromToken(_authHelper.GetToken() ?? "") ?? throw new NotFoundException("User not found. Place use a valid token.");
+        var user = await _authHelper.GetUserFromToken() ?? throw new NotFoundException("User not found. Place use a valid token.");
         var orders = await _selectSchedule.OrdersSince(DateTime.Now.AddMonths(-3), user.Id);
 
         return Ok(new ManyItems<OrderResponse>(orders.Select(OrderResponse.FromOrder)));
@@ -50,7 +50,7 @@ public class ScheduleController : ControllerBase
     [HttpGet("Parties")]
     public async Task<ActionResult<ManyItems<ScheduleResponse>>> GetParties()
     {
-        var user = await _authHelper.GetUserFromToken(_authHelper.GetToken() ?? "") ?? throw new NotFoundException("User not found. Place use a valid token.");
+        var user = await _authHelper.GetUserFromToken() ?? throw new NotFoundException("User not found. Place use a valid token.");
 
         var parties = await _selectSchedule.FromUser(user.Id);
 
@@ -60,6 +60,8 @@ public class ScheduleController : ControllerBase
     [HttpGet("Party/{scheduleid:Guid}")]
     public async Task<ActionResult<ScheduleResponse>> GetParty(Guid scheduleid)
     {
+        var _ignored = await _authHelper.GetUserFromToken() ?? throw new NotFoundException("User not found. Place use a valid token.");
+
         var party = await _selectSchedule.FindById(scheduleid) ?? throw new NotFoundException("Festa não encontrada.");
 
         return Ok(ScheduleResponse.FromParty(party));
