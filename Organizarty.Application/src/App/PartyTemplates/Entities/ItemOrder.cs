ffd20ded8f8cce47f7c5a86dee3b67ca1c7cb794
Débora Liberato <91430446+De004ra@users.noreply.github.com>
2023-12-05 04:49:35 +0000

    socorro deus

--- Organizarty.Application/src/App/PartyTemplates/Entities/ItemOrder.cs
+++ Organizarty.Application/src/App/PartyTemplates/Entities/ItemOrder.cs
@@ -1,5 +1,7 @@
 using Organizarty.Application.App.Party.Enums;
 namespace Organizarty.Application.App.Party.Entities;
 
-public record ItemOrder(Guid id, ItemType type, string name, int quantity, string note, decimal price, Guid partyId);
+public record ItemOrder(Guid id, ItemType type, string name, int quantity, string note, decimal price, Guid partyId){
+    public decimal TotalPrice => price * quantity;
+}
 
