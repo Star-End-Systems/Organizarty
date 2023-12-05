2f0d446004377551f3f85123725f350bc8b0e51b
lu-css <dossantossilvalucas963@gmail.com>
2023-12-05 05:53:06 -0300

    list colors

--- Organizarty.UI/Pages/Clients/Decorations/Dashboard/Edit.cshtml.cs
+++ Organizarty.UI/Pages/Clients/Decorations/Dashboard/Edit.cshtml.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Organizarty.Adapters;
 using Organizarty.Application.App.DecorationTypes.Entities;
 using Organizarty.Application.App.Decorations.Entities;
-using Organizarty.Application.App.Decorations.Entities;
 
 using Organizarty.Application.App.DecorationTypes.UseCases;
 
 using Organizarty.UI.Attributes;
+using Organizarty.Application.App.DecorationInfos.UseCases;
+using Organizarty.Application.App.DecorationInfos.Entities;
+
 namespace Organizarty.UI.Pages.Clients.Decorations.Dashboard;
 
 [Authorized("/Clients/Accounts/Login", UserType.Client)]
@@ -17,23 +19,20 @@ public class EditDashboardModel : PageModel
     public DecorationCategory DecorationCategory { get; set; }
 
     public SelectDecorationTypeUseCase _selectDecoration {get; set;}
-    public SelectDecorationInfoUseCase _selectInfoDecoration {get; set;}
+    public SelectDecorationItemUseCase _selectInfoDecoration {get; set;}
 
     public DecorationType Decoration {get; set;} = new();
     public List<DecorationInfo> Decorations {get; set;} = new();
 
-    public EditDashboardModel(SelectDecorationTypeUseCase selectDecoration){
+    public EditDashboardModel(SelectDecorationTypeUseCase selectDecoration, SelectDecorationItemUseCase selectInfoDecoration)
+    {
         _selectDecoration = selectDecoration;
+        _selectInfoDecoration = selectInfoDecoration;
     }
 
     public async Task OnGetAsync(Guid decorationId)
     {
         Decoration = await _selectDecoration.FindByIdWithItems(decorationId);
         Decorations = await _selectInfoDecoration.ListFromType(decorationId);
-        Console.WriteLine(Decoration.Decorations.Count);
-        foreach (var item in Decoration.Decorations)
-        {
-            Console.WriteLine(item);
-        }
     }
 }
