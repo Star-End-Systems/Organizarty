9ac140bbb948e05bff50bb6388d74593429a4880
lu-css <dossantossilvalucas963@gmail.com>
2023-12-30 23:57:44 -0300

    [feat] split the controller into varios file

--- Organizarty.UI/Controllers/Users/UserControllerDto.cs
+++ Organizarty.UI/Controllers/Users/UserControllerDto.cs
@@ -1,4 +1,5 @@
 namespace Organizarty.UI.Controllers.Users;
 
 public record UserCreateResponse(string Email);
+
 public record UserLoginResponse(Guid id, string Email, string Fullname, string Username, string Token);
